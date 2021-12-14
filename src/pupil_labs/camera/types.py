7883886a47585aaf51a4eecd59f4c93f554e4f91
Roman Roibu <romanroibu@hey.com>
2021-12-14 11:20:15 +0100

    Update camera types

--- src/pupil_labs/camera/types.py
+++ src/pupil_labs/camera/types.py
@@ -1,8 +1,8 @@
 import nptyping as npt
 import typing as T
 
-CameraMatrix = npt.NDArray[3, 3, npt.Float[32]]
+CameraMatrix = npt.NDArray[(3, 3), npt.Float[32]]
 DistCoeffs = npt.NDArray[T.Any, npt.Float[32]]
-Image = npt.NDArray  #TODO: Provide dimensions
-Points2D = npt.NDArray  #TODO: Provide dimensions
-Points3D = npt.NDArray  #TODO: Provide dimensions
+Image = npt.NDArray[(T.Any, T.Any), npt.UInt[8]]
+Points2D = npt.NDArray[(T.Any, 2), npt.Float[32]]
+Points3D = npt.NDArray[(T.Any, 3), npt.Float[32]]
