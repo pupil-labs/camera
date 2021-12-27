4b76e7c7d6bcd155a58dd82c158880ec52db015c
Roman Roibu <romanroibu@hey.com>
2021-12-27 12:41:44 +0100

    Add Optimization type

--- src/pupil_labs/camera/types.py
+++ src/pupil_labs/camera/types.py
@@ -1,3 +1,4 @@
+import enum
 import typing as T
 
 import nptyping as npt
@@ -7,3 +8,13 @@ DistCoeffs = npt.NDArray[T.Any, npt.Float[32]]
 Image = npt.NDArray[(T.Any, T.Any), npt.UInt[8]]
 Points2D = npt.NDArray[(T.Any, 2), npt.Float[32]]
 Points3D = npt.NDArray[(T.Any, 3), npt.Float[32]]
+
+
+class AutoLowercaseNameEnum(enum.Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name.lower()
+
+
+class Optimization(AutoLowercaseNameEnum):
+    ACCURACY = enum.auto()
+    SPEED = enum.auto()
