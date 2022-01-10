6747a13c5f59f92515d065e261ec160d241172bf
Roman Roibu <romanroibu@hey.com>
2022-01-10 10:21:15 +0100

    Implement project_points and unproject_points for mpmath backend

--- src/pupil_labs/camera/camera_mpmath.py
+++ src/pupil_labs/camera/camera_mpmath.py
@@ -1,4 +1,6 @@
 from mpmath import mp
+import numpy as np
+import typing as T
 
 from . import types as CT
 from . import utils
@@ -17,24 +19,61 @@ class CameraRadial(Base):
     def unproject_points(
         self, points_2d: CT.Points2D, use_distortion: bool = True
     ) -> CT.Points3D:
-        raise NotImplementedError()  # FIXME
+        if len(points_2d) == 0:
+            return np.asarray([], dtype=np.float32)
+
+        camera_matrix = self.camera_matrix
+
+        if use_distortion:
+            dist_coeffs = self.dist_coeffs
+        else:
+            dist_coeffs = [0.0, 0.0, 0.0, 0.0, 0.0]
+
+        if len(dist_coeffs) == 5:
+            dist_coeffs += [0.0, 0.0, 0.0]
+
+        def unproject_point(point_2d: T.Tuple[float, float]):
+            x, y, z = *point_2d, 1.
+            x = (x - camera_matrix[0][2]) / camera_matrix[0][0]
+            y = (y - camera_matrix[1][2]) / camera_matrix[1][1]
+
+            result = mp.findroot(
+                lambda x_, y_: utils.apply_distortion_model((x_, y_), dist_coeffs)
+                - np.asarray([x, y]),
+                (x, y),
+                verbose=False,
+                multidimensional=True,
+                tol=1e-4,
+                solver="muller",
+            )
+            x, y = float(result[0]), float(result[1])
+            return x, y, z
+
+        points_3d = [unproject_point(p) for p in points_2d]
+        return np.asarray(points_3d, dtype=np.float32)
 
     def project_points(
         self, points_3d: CT.Points3D, use_distortion: bool = True
     ) -> CT.Points2D:
         if len(points_3d) == 0:
-            return []
+            return np.asarray([], dtype=np.float32)
 
         camera_matrix = self.camera_matrix
-        dist_coeffs = self.dist_coeffs
 
-        if len(points_3d) == 5:
+        if use_distortion:
+            dist_coeffs = self.dist_coeffs
+        else:
+            dist_coeffs = [0.0, 0.0, 0.0, 0.0, 0.0]
+
+        if len(dist_coeffs) == 5:
             dist_coeffs += [0.0, 0.0, 0.0]
 
-        def project_point(point):
-            x_, y_ = utils.apply_distortion_model(point, dist_coeffs)
-            x_ = x_ * camera_matrix[0][0] + camera_matrix[0][2]
-            y_ = y_ * camera_matrix[1][1] + camera_matrix[1][2]
-            return x_, y_
+        def project_point(point_3d: T.Tuple[float, float, float]):
+            x, y, _ = point_3d
+            x, y = utils.apply_distortion_model((x, y), dist_coeffs)
+            x = x * camera_matrix[0][0] + camera_matrix[0][2]
+            y = y * camera_matrix[1][1] + camera_matrix[1][2]
+            return x, y
 
-        return list(map(project_point, points_3d))
+        points_2d = [project_point(p) for p in points_3d]
+        return np.asarray(points_2d, dtype=np.float32)
