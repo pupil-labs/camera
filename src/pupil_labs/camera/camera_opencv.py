1f611cfbd92d3a9178aa931f7e1bff3f90253468
Roman Roibu <romanroibu@hey.com>
2022-01-10 10:21:23 +0100

    Implement project_points and unproject_points for opencv backend

--- src/pupil_labs/camera/camera_opencv.py
+++ src/pupil_labs/camera/camera_opencv.py
@@ -20,14 +20,17 @@ class CameraRadial(Base):
         if use_distortion:
             dist_coeffs = self.dist_coeffs
         else:
-            dist_coeffs = np.asarray([[0.0, 0.0, 0.0, 0.0, 0.0]])
+            dist_coeffs = [0.0, 0.0, 0.0, 0.0, 0.0]
 
+        dist_coeffs = np.asarray(dist_coeffs, dtype=np.float32)
+        camera_matrix = np.asarray(self.camera_matrix, dtype=np.float32)
+
+        points_2d = np.asarray(points_2d, dtype=np.float32)
         points_2d = points_2d.reshape((-1, 1, 2))
-        points_2d = cv2.undistortPoints(points_2d, self.camera_matrix, dist_coeffs)
+        points_2d = cv2.undistortPoints(points_2d, camera_matrix, dist_coeffs)
 
         points_3d = cv2.convertPointsToHomogeneous(points_2d)
         points_3d = points_3d.reshape((-1, 3))
-
         return points_3d
 
     def project_points(
@@ -36,16 +39,19 @@ class CameraRadial(Base):
         if use_distortion:
             dist_coeffs = self.dist_coeffs
         else:
-            dist_coeffs = np.asarray([[0.0, 0.0, 0.0, 0.0, 0.0]])
+            dist_coeffs = [0.0, 0.0, 0.0, 0.0, 0.0]
 
         rvec = np.zeros((1, 1, 3))
         tvec = np.zeros((1, 1, 3))
 
+        dist_coeffs = np.asarray(dist_coeffs, dtype=np.float32)
+        camera_matrix = np.asarray(self.camera_matrix, dtype=np.float32)
+
+        points_3d = np.asarray(points_3d, dtype=np.float32)
         points_3d = points_3d.reshape((1, -1, 3))
 
         points_2d, _ = cv2.projectPoints(
-            points_3d, rvec, tvec, self.camera_matrix, dist_coeffs
+            points_3d, rvec, tvec, camera_matrix, dist_coeffs
         )
         points_2d = points_2d.reshape((-1, 2))
-
         return points_2d
